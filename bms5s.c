dba290b9c865b6150dc662e1d6b43ad9195a3efc
Harris Pagonis <hpagonis@hotmail.com>
2022-08-12 01:32:44 +0300

    Add monitor and adc command, remove tov0 interrupt

--- bms5s.c
+++ bms5s.c
@@ -9,9 +9,6 @@
 #include <avr/interrupt.h>
 #include <avr/eeprom.h>
 
-#define BAUD 9600
-#include <util/setbaud.h>
-
 #define OV (4200L * 1024) / 5000
 #define UV (3000L * 1024) / 5000
 #define CELL_NUMBER 5
@@ -19,6 +16,8 @@
 #define CMD_SET_SLAVE_ADDRESS 0x10
 #define CMD_READ_VOLTAGES 0x20
 #define CMD_SET_OFFSETS 0x30
+#define CMD_MONITOR 0x40
+#define CMD_ADC 0x50
 
 uint16_t cellv[CELL_NUMBER];
 uint8_t dc_pin[CELL_NUMBER] = {PIND4, PIND3, PIND2, PIND5, PIND6};
@@ -26,7 +25,8 @@ int8_t offset[CELL_NUMBER];
 uint8_t volatile address;
 uint8_t EEMEM e_address = 3;
 int8_t EEMEM e_offset[CELL_NUMBER] = {9, 7, -10, 14, 4};
-uint8_t volatile cmd;
+uint8_t volatile cmd, data;
+uint8_t  f_monitor = 0;
 
 void transmit_voltage(uint16_t voltage);
 
@@ -41,9 +41,9 @@ int main() {
   }
 
   // Initialize Hysterisis Timer
-  // around 30Hz
+  // around 8Hz
   TCCR0 = _BV(CS02) | _BV(CS00);
-  TIMSK = _BV(TOIE0);
+  //TIMSK = _BV(TOIE0);
 
   // Initialize ADC
   // Set AVcc as voltage reference, enable ADC and interrupt
@@ -53,13 +53,8 @@ int main() {
 
   // Initialize UART
   // Enable MPCM mode at 9600 baud 9N1
-  #if USE_2X
-  UCSRA = _BV(MPCM) | _BV(U2X);
-  #else
+  UBRRL = 12;
   UCSRA = _BV(MPCM);
-  #endif
-  //UBRRH = 0;
-  //UBRRL = 12;
   UCSRB = _BV(RXCIE) | _BV(RXEN) | _BV(TXEN) | _BV(UCSZ2) | _BV(TXB8);
   UCSRC = _BV(URSEL) | _BV(UCSZ1) | _BV(UCSZ0);
 
@@ -70,40 +65,63 @@ int main() {
   sei();
 
   while(1) {
+    if (TIFR & _BV(TOV0) && ADCSRA & _BV(ADEN)) {
     // Read cell voltages
-    for (uint8_t i = 0; i < CELL_NUMBER; i++) {
-      ADMUX &= 0xF0;
-      ADMUX |= CELL_NUMBER - i - 1;
-      ADCSRA |= _BV(ADSC);
-      while (ADCSRA & _BV(ADSC));
-      uint8_t *pcellv = (uint8_t *)&cellv[i];
-      *pcellv = ADCL;
-      pcellv++;
-      *pcellv = ADCH;
-      cellv[i] += offset[i];
+      for (uint8_t i = 0; i < CELL_NUMBER; i++) {
+        ADMUX &= 0xF0;
+        ADMUX |= CELL_NUMBER - i - 1;
+        ADCSRA |= _BV(ADSC);
+        while (ADCSRA & _BV(ADSC));
+        uint8_t *pcellv = (uint8_t *)&cellv[i];
+        *pcellv = ADCL;
+        pcellv++;
+        *pcellv = ADCH;
+        cellv[i] += offset[i];
 
-      if (cellv[i] > OV) {
-        if (i == 0) {
-          PORTD |= _BV(dc_pin[i]);
+        if (cellv[i] > OV) {
+          if (i == 0) {
+            PORTD |= _BV(dc_pin[i]);
+          } else {
+            PORTD &= ~_BV(dc_pin[i]);
+          }
         } else {
-          PORTD &= ~_BV(dc_pin[i]);
+          if (i == 0) {
+            PORTD &= ~_BV(dc_pin[i]);
+          } else {
+            PORTD |= _BV(dc_pin[i]);
+          }
         }
-      } 
+
+        if (f_monitor) {
+          if (i == 0) transmit_voltage(0xFFFF);
+          transmit_voltage(cellv[i]);
+        }
+      }
+      TIFR = _BV(TOV0);
     }
 
-    if (cmd) {
-      uint8_t cmd_part = cmd & 0xF0;
-      uint8_t data_part = cmd & 0x0F;
-      if (cmd_part == CMD_SET_SLAVE_ADDRESS) {
-        address = data_part;
-        eeprom_write_byte(&e_address, data_part);
+    if (cmd && (UCSRA & _BV(MPCM))) {
+      if (cmd == CMD_SET_SLAVE_ADDRESS) {
+        address = data;
+        eeprom_write_byte(&e_address, data);
       }
-      if (cmd_part == CMD_SET_OFFSETS) {
-        // TODO
+      if ((cmd & 0xf0) == CMD_SET_OFFSETS) {
+        offset[cmd & 0x0f] = (int8_t) data;
+        eeprom_update_block((const void*) offset, (void *) e_offset, CELL_NUMBER);
       }
-      if (cmd_part == CMD_READ_VOLTAGES) {
+      if (cmd == CMD_READ_VOLTAGES) {
         for (uint8_t i = 0; i < CELL_NUMBER; i++) {
           transmit_voltage(cellv[i]);
+        }
+      }
+      if (cmd == CMD_MONITOR) {
+        f_monitor = data;
+      }
+      if (cmd == CMD_ADC) {
+        if (data) {
+          ADCSRA |= _BV(ADEN);
+        } else {
+          ADCSRA &= ~_BV(ADEN);
         }
       }
       cmd = 0;
@@ -118,17 +136,17 @@ void transmit_voltage(uint16_t voltage) {
   UDR = (uint8_t) (voltage >> 8);
 }
 
-ISR(TIMER0_OVF_vect) {
-  static uint8_t counter = 0;
-  counter++;
-  if (counter == 15) {
-    for (int i=1; i<CELL_NUMBER; i++) {
-      PORTD |= _BV(dc_pin[i]);
-    }
-    PORTD &= ~_BV(dc_pin[0]);
-    counter = 0;
-  }
-}
+// ISR(TIMER0_OVF_vect) {
+//   static uint8_t counter = 0;
+//   counter++;
+//   if (counter == 15) {
+//     for (int i=1; i<CELL_NUMBER; i++) {
+//       PORTD |= _BV(dc_pin[i]);
+//     }
+//     PORTD &= ~_BV(dc_pin[0]);
+//     counter = 0;
+//   }
+// }
 
 ISR(USART_RXC_vect) {
   // Determine if address or data
@@ -137,8 +155,12 @@ ISR(USART_RXC_vect) {
       UCSRA = 0;
     }
   } else {
-    cmd = UDR;
-    UCSRA = _BV(MPCM);
+    if (cmd) {
+      data = UDR;
+      UCSRA = _BV(MPCM);
+    } else {
+      cmd = UDR;
+    }
   }
 }
 
